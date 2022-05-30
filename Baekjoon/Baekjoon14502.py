0a4759a5bb73c6d0c4ac170f897ea1a2624ee2bd
jy9922 <myjjy6190@gmail.com>
2022-05-31 03:12:01 +0900

    solve: [#9] 백준 silver3 퇴사

---
+++ Baekjoon/Baekjoon14502.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = []
+
+for i in range(n):
+  graph.append(list(map(int, input().split())))
+
+cost_box = [0]
+
+def bfs(day, cost):
+  q = deque()
+  q.append((day, cost))
+  pay = cost
+  while q:
+    days, cost = q.popleft()
+    x = days + graph[days-1][0]
+    if x <= n+1:
+      cost_box.append(pay)
+      for i in range(x, n+1):
+        if i + graph[i-1][0] > n+1:
+          continue
+        else:
+          cost_box.append(pay)
+          pay = cost + graph[i-1][1]
+          q.append((i, pay))
+          cost_box.append(pay)
+
+for i in range(1, n+1):
+  bfs(i, graph[i-1][1])
+
+print(max(cost_box))
