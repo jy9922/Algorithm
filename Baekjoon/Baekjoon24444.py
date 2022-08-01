c035da57ed85ce6b721afca3617196dfd373722b
jy_9922 <myjjy6190@gmail.com>
2022-08-01 10:32:47 +0900

    solve: 백준 silver 2 알고리즘 수업

---
+++ Baekjoon/Baekjoon24444.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for j in graph:
+    j.sort(reverse=True)
+
+
+def bfs(graph, start):
+    cnt = 1
+    q = deque()
+    q.append(start)
+    visited[start] = cnt
+    cnt += 1
+    while q:
+        new = q.popleft()
+        for k in graph[new]:
+            if visited[k] == 0:
+                q.append(k)
+                visited[k] = cnt
+                cnt += 1
+
+
+bfs(graph, r)
+for i in visited[1::]:
+    print(i)
